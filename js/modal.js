4bc28ec81702f093cbcd7f472d345a27ff9b002b
John Collins <j.k.collins410@gmail.com>
2019-08-14 10:33:15 -0400

    Updated constructor modal.js to include all needed information for the cards Added the createCard function to modal.js Added the createContent function to modal.js
    Moved content in script.js to modal.js where appropriate for creating the card content
    
    Began working on the modal-container for handling and displaying the modal card upon a click event

---
+++ js/modal.js
@@ -0,0 +1,40 @@
+class Modal{
+  constructor (image, firstName, lastName, email, city, state, phone, dob, nat){
+    this.image = image;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email= email;
+    this.city = city;
+    this.state = state;
+    this.phone = phone;
+    this.dob = dob;
+    this.nat = nat;
+  }
+  //creates each
+  createCard () {
+    //Creates the gallery container
+    $('#gallery')
+        .append(`<div class="card" id="card_${this.firstName}_${this.lastName}">`);
+
+    //Creates the image and information containers within the card
+    $(`#card_${this.firstName}_${this.lastName}`)
+        .append(`<div class="card-img-container" id="card_img_${this.firstName}_${this.lastName}">`)
+        .append(`<div class="card-info-container" id="card_info_${this.firstName}_${this.lastName}">`);
+  }
+
+  createContent(){
+    //Insert the picture holder into the card
+    $(`#card_img_${this.firstName}_${this.lastName}`).prop('src',`${this.image}`);
+    $(`#card_info_${this.firstName}_${this.lastName}`).prop('src',`${this.image}`);
+
+    //Inserts the profile picture in the image holder for each card
+    $(`#card_img_${this.firstName}_${this.lastName}`)
+        .append(`<img class="card-img" src="${this.image}" alt="profile picture" id="${this.cardCount}">`);
+
+    //Inserts the profile info in the card info container
+    $(`#card_info_${this.firstName}_${this.lastName}`)
+        .append(`<h3 id="name" class="card-name cap" id="card_">${this.firstName} ${this.lastName}</h3>`)
+        .append(`<p class="card-text" id="card_${this.cardCount}">${this.email}</p>`)
+        .append(`<p class="card-text cap" id="card_${this.cardCount}">${this.city}, ${this.state}</p>`);
+  }
+}
--- js/script.js
+++ js/script.js
@@ -6,81 +6,23 @@ $('.search-container')
     .append('<input type="search" id="search-input" class="search-input" placeholder="Search...">')
     .append('<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">');
 
-//Creates the gallery container
-$('#gallery')
-    .append('<div class="card">');
 
-//Creates the image and information containers
-$('.card')
-    .append('<div class="card-img-container">')
-    .append('<div class="card-info-container">');
 
-//Inserts the profile picture in the image container for each card
-$('.card-img-container')
-    .append('<img class="card-img" src="https://placehold.it/90x90" alt="profile picture">');
-
-//Inserts the profile info in the card info container
-$('.card-info-container')
-    .append('<h3 id="name" class="card-name cap">first last</h3>')
-    .append('<p class="card-text">email</p>')
-    .append('<p class="card-text cap">city, state</p>');
-
-//Creates the Modal container div
-$('body')
-    .append('<div class="modal-container">');
-
-//Adds content to the modal container div
-$('.modal-container')
-    .append('<div class="modal">')
-    .append('<div class="modal-btn-container">');
-
-//Attaches a button to the modal for closing the div
-$('.modal')
-    .append('<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>')
-    .append('<div class="modal-info-container">');
-
-//Adds the picture to the modal info container
-$('.modal-info-container')
-    .append('<img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">')
-    .append('<h3 id="name" class="modal-name cap">name</h3>')
-    .append('<p class="modal-text">email</p>')
-    .append('<p class="modal-text cap">city</p>')
-    .append('<hr>')
-    .append('<p class="modal-text">(555) 555-5555</p>')
-    .append('<p class="modal-text">123 Portland Ave., Portland, OR 97204</p>')
-    .append('<p class="modal-text">Birthday: 10/21/2015</p>');
 /**********************************************************************************
 Code after this point handles the interaction of data and on screen characteristics
 ***********************************************************************************/
+let modal;
 
 // Hide the modal container until a card is clicked
 $('.modal-container').hide();
 
-//Send request for json data
-// let xhr = new XMLHttpRequest();
-// xhr.onreadystatechange= function (){
-//   if(xhr.readyState === 4){
-//     let modal = JSON.parse(xhr.responseText);
-//     for(let i=0; i < modal.length; i++){
-//       modal[i].img;
-//       modal[i].firstName;
-//       modal[i].lastName;
-//       modal[i].email;
-//       modal[i].loc;
-//       modal[i].ph;
-//       modal[i].birthday;
-//     }
-//   }
-// };
-// xhr.open('GET', 'data/modal.json');
-// xhr.send();
 
 $.ajax({
-  url: 'https://randomuser.me/api/1.2/?nat=us&inc=gender,name,location,email,dob,cell,id,picture,nat',
+  url: 'https://randomuser.me/api/1.2/?results=12&nat=us&inc=gender,name,location,email,dob,cell,id,picture,nat',
   dataType: 'json',
+  async: 'false',
   success: function(data) {
     data.results.forEach( dataModal => {
-      const modal = dataModal;
       let image = dataModal.picture.medium;
       let firstName = dataModal.name.first;
       let lastName = dataModal.name.last;
@@ -90,17 +32,37 @@ $.ajax({
       let phone = dataModal.cell;
       let dob = /[0-9]{4}-[0-9]{2}-[0-9]{2}/.exec(dataModal.dob.date)[0];
       let nat = dataModal.nat;
-      createImage(image);
-      placeFName(firstName);
+      modal = new Modal(`${image}`,`${firstName}`,`${lastName}`,`${email}`
+          ,`${city}`,`${state}`,`${phone}`,`${dob}`,`${nat}`);
+      modal.createCard();
+      modal.createContent();
     })
   }
 });
 
-function createImage(image){
-  $('.card-img').prop('src',`${image}`);
-  $('.modal-info-container').prop('src',`${image}`);
-}
+console.log($('.card'));
+//Creates the modal popup container
+function createInfoContainer(e){
+  //Creates the Modal container div
+  $('body')
+      .append('<div class="modal-container">');
+  $('.modal-container')
+      .append('<div class="modal">')
+      .append('<div class="modal-btn-container">');
 
-function placeFName(){
-  $('#name')
+  //Attaches a button to the modal for closing the div
+  $('.modal')
+      .append('<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>')
+      .append('<div class="modal-info-container">');
+  //Adds the picture to the modal info container
+  $('.modal-info-container')
+      .append('<img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">')
+      .append('<h3 id="name" class="modal-name cap">name</h3>')
+      .append('<p class="modal-text">email</p>')
+      .append('<p class="modal-text cap">city</p>')
+      .append('<hr>')
+      .append('<p class="modal-text">(555) 555-5555</p>')
+      .append('<p class="modal-text">123 Portland Ave., Portland, OR 97204</p>')
+      .append('<p class="modal-text">Birthday: 10/21/2015</p>');
+
 }
